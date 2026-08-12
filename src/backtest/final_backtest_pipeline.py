1a9824599a735d1dcdd4571667fa1f94e25c8544
Daniyal <syeddaniyal1016@gmail.com>
2026-08-12 06:45:15 +0530

    Forget to import os

--- src/backtest/final_backtest_pipeline.py
+++ src/backtest/final_backtest_pipeline.py
@@ -2,7 +2,7 @@ import json
 from pathlib import Path
 import numpy as np
 import pandas as pd
-
+import os
 # =====================================================================
 # GLOBAL BACKTEST CONFIGURATION
 # =====================================================================
